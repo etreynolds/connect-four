eb36b49f8c244bee09f586de206e04405bb4fdb5
Ethan Reynolds <ethan.t.reynolds@gmail.com>
2022-11-15 18:31:30 -0700

    add fonts

---
+++ app.js
@@ -0,0 +1,8 @@
+/*
+7 wide by 6 deep board
+2 players (red and blue)
+alternating colors (or player turns)
+piece falls down to furthest unoccupied slot
+game ends when player makes four in a row (horizontal, vertical, diagonal)
+game is tie if entire board fills up without winner
+*/
